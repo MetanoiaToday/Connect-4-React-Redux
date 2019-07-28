d1b83508e3b7384eca200d3edd4ad0a7461776b7
Debra Toranska <48626342+WhereNatureEnds@users.noreply.github.com>
2019-07-29 00:20:32 +0100

    almost there

---
+++ connect4/src/store.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+//initial state for my store
+const initial = {
+    current: 'red',
+    board: [
+        [], //col 0
+        [], //col 1
+        [], //col 2
+        [], //col 3
+        [], //col 4
+        [], //col 5
+        [], //col 6
+    ],
+};
+
+function reducer(state, action) {
+    if (action.type === 'DROP_TILE') {
+        console.log('dropping onto col' + action.payload);
+        const tile = state.current;
+        const col = state.board[action.payload].concat(tile); //our new col
+        const board = state.board.slice();
+        board[action.payload] = col;
+
+        return {
+            current: state.current === 'red' ? 'black' : 'red',
+            board: board
+        }
+
+
+    }
+    return state;
+
+}
+
+export default createStore(reducer, initial);
